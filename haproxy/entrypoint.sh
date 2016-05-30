859aca78f0402428802128b481a557d6a480e3f8
thomassc <thomsch98@gmail.com>
2016-05-30 08:20:36 +0200

    providing haproxy for loadbalancing (consul & vault)

---
+++ haproxy/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+for kv in $(env) ; do
+    key=${kv%%=*}
+    val=${kv##*=}
+    if [ -n "$(echo $key | grep '^DM_.*')" ] ; then
+        consul="$consul\n\tserver $key $val:18500 maxconn 32"
+        vault="$vault\n\tserver $key $val:18200 maxconn 32"
+    fi
+done        
+
+cat <<EOT > /usr/local/etc/haproxy/haproxy.cfg
+# Simple configuration for an HTTP proxy listening on port 80 on all
+# interfaces and forwarding requests to a single backend "servers" with a
+# single server "server1" listening on 127.0.0.1:8000
+global
+    daemon
+    maxconn 256
+
+defaults
+    mode http
+    timeout connect 5000ms
+    timeout client 50000ms
+    timeout server 50000ms
+
+frontend consul-http-in
+    bind *:8500
+    default_backend consul-servers
+
+backend consul consul-servers
+EOT
+echo $consul >> /usr/local/etc/haproxy/haproxy.cfg 
+
+cat <<EOT >> /usr/local/etc/haproxy/haproxy.cfg
+frontend vault-http-in
+    bind *:8200
+    default_backend vault-servers
+
+backend vault-servers
+EOT
+echo $vault >> /usr/local/etc/haproxy/haproxy.cfg 
+
+exec "$@"
